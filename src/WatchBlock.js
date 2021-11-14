9ed11065a862c4840a194c14a4d13994d0b77043
Marina Dariy <dariy.mvs@gmail.com>
2021-11-14 13:50:23 +0300

    initial commit

---
+++ src/WatchBlock.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import WatchInput from './WatchInput';
+import moment from 'moment';
+import './Watchblock.css';
+
+export default function WatchBlock() {
+  let [ watchArr, setWatch ] = useState([]);
+
+  const changeWatchArr = (name, zone, id) => {
+    const watch = {
+      name: name,
+      zone: zone,
+      id: id,
+      moment: moment.utc().utcOffset(zone).format('HH:mm:ss')
+    }
+    setWatch(prev => ([...prev, watch]));
+  }
+
+  const deleteWatch = (name, id) => {
+    setWatch(prev => {
+      let newWatchArr = [];
+      prev.forEach(el => {
+        if (el.name === name && el.id === id) {
+          return;
+        }
+        newWatchArr.push(el);
+      });
+      return newWatchArr;
+    });
+  }
+
+  useEffect(() => {
+    let timeout;
+    if (watchArr.length !== 0) {
+    timeout = setTimeout(() => {
+      setWatch(prev => {
+        let newState = [];
+        prev.forEach(el => {
+          let newEl = {...el};
+          newEl.moment = moment.utc().utcOffset(el.zone).format('HH:mm:ss');
+          newState.push(newEl);
+        });
+        return newState;
+      });
+    }, 1000);
+    }
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [watchArr]);
+  console.log(watchArr);
+  let watchList = watchArr.map((el) => {
+    const {name, id, zone} = el;
+    return <li data-timezone={zone} key={id}>
+      <span className="name">{name}</span>
+      <span className="time">{moment.utc().utcOffset(zone).format('HH:mm:ss')}</span>
+      <button onClick={deleteWatch.bind(this, name, id)}>X</button>
+    </li>
+  });
+  
+  return (
+    <div className="container">
+      <WatchInput changeWatch={changeWatchArr} />
+      <ul className="watchList">
+        {watchList}
+      </ul>
+    </div>
+  )
+}
---
+++ src/WatchInput.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+export default function WatchInput(props) {
+  const { changeWatch } = props
+  let [ watchName, setWatchName ] = useState('');
+  let [ watchTimezone, setWatchCorr ] = useState('');
+  const getInputValue = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    if(name === 'name') {
+      setWatchName(value);
+    } else {
+
+    }
+    name === 'name' ? setWatchName(value) : setWatchCorr(value);
+  }
+
+  const changeWatchArr = () => {
+    const id = nanoid(4);
+
+    if (/^(?:Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])$/.test(watchTimezone)) {
+      changeWatch(watchName, watchTimezone, id);
+    } else {
+      alert('проверьте правильность введённой таймзоны. Введите отклонение в формате "+/-00:00".');
+    }
+  }
+
+  return (
+    <div className="inputBox">
+      <input className="name" name="name" onChange={getInputValue}/>
+      <input className="time" name="time" onChange={getInputValue}/>
+      <button className="inputBtn" onClick={changeWatchArr}>добавить</button>
+    </div>
+  )
+}
